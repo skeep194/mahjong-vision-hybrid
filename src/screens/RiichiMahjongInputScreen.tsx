3b6f483ed0285d9915159fbc70a4bd0b7fbfc2e4
JongunJeong <skeep194@knu.ac.kr>
2024-07-26 14:49:38 +0900

    feat: input screen animation

--- src/screens/RiichiMahjongInputScreen.tsx
+++ src/screens/RiichiMahjongInputScreen.tsx
@@ -1,6 +1,7 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Button, Icon, IconElement, Input, Layout, Text } from '@ui-kitten/components';
 import React, { useState } from 'react';
+import { FadeInView } from 'src/components/FadeinView';
 import { HandImage } from 'src/components/HandImage';
 import { HandProtocolHelp } from 'src/components/HandProtocolHelp';
 import { RootStackParamList } from 'src/types';
@@ -16,18 +17,51 @@ const PictureIcon = (props: any): IconElement => (
 )
 
 export const RiichiMahjongInputScreen = ({ navigation, route }: RiichiMahjongInputScreenProps) => {
-  const [handString, handUpdate] = useState<string>('')
+  const [handString, handUpdate] = useState<string>('');
+  const [inputLevel, inputLevelUpdate] = useState<number>(0);
+  const [handInformations, handInformationsUpdate] = useState<string[]>([]);
+  const NextButton = () => (
+    <MarginButton status='info' appearance='outline' onPress={() => {
+      inputLevelUpdate(inputLevel + 1);
+      handInformations.push(handString);
+      handInformationsUpdate(handInformations);
+      handUpdate('');
+    }}>
+      다음
+    </MarginButton>
+  )
+  const CompleteButton = () => (
+    <MarginButton status='success' onPress={() => {
+      handInformations.push(handString);
+      handInformationsUpdate(handInformations);
+      console.log('need api call implement');
+      console.log(`hand informations: ${handInformations}`);
+    }}>
+      완료
+    </MarginButton>
+  )
+  const changeUpdte = (text: string) => {
+    handUpdate(text);
+  }
+
   return <Main>
-    <Input placeholder='손패 (후로 제외)' onChangeText={(text: string) => {
-      handUpdate(text);
-    }} />
+    <MarginInput disabled={inputLevel > 0} placeholder='손패 (후로 제외)' onChangeText={changeUpdte} />
+    {inputLevel >= 1 && <FadeInView style={{}}><MarginInput placeholder='후로패' disabled={inputLevel > 1} onChangeText={changeUpdte} /></FadeInView>}
+    {inputLevel >= 2 && <FadeInView style={{}}><MarginInput placeholder='도라표지패' disabled={inputLevel > 2} onChangeText={changeUpdte} /></FadeInView>}
+    {inputLevel >= 3 && <FadeInView style={{}}><MarginInput placeholder='화료패(론, 쯔모 선언패)' disabled={inputLevel > 3} onChangeText={changeUpdte} /></FadeInView>}
     <HandImage handString={handString} />
-    <InputButton status='basic' accessoryLeft={PictureIcon}>
+    <MarginButton status='basic' accessoryLeft={PictureIcon}>
       사진으로 입력
-    </InputButton>
-    <InputButton status='success'>
-      완료!
-    </InputButton>
+    </MarginButton>
+    {inputLevel === 3 ? <CompleteButton /> : <NextButton />}
+    {inputLevel >= 1 && <MarginButton status='danger' appearance='outline' onPress={() => {
+      inputLevelUpdate(inputLevel - 1);
+      handUpdate(handInformations[handInformations.length - 1]);
+      handInformations.pop();
+      handInformationsUpdate(handInformations);
+    }}>
+      이전
+    </MarginButton>}
     <HandProtocolHelp />
   </Main>
 }
@@ -36,7 +70,10 @@ const Main = styled(Layout)`
   padding: 20px;
   height: 100%;
 `
-const InputButton = styled(Button)`
+const MarginButton = styled(Button)`
   padding: 5px;
-  margin: 10px;  
+  margin-top: 10px;
+`
+const MarginInput = styled(Input)`
+  margin-top: 10px;
 `
