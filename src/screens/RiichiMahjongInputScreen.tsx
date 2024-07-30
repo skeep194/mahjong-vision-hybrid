30263d0b52a7d73d2c18cc1418a203478ceffc74
JongunJeong <skeep194@knu.ac.kr>
2024-07-30 17:56:11 +0900

    feat: input screen UI implement

--- src/screens/RiichiMahjongInputScreen.tsx
+++ src/screens/RiichiMahjongInputScreen.tsx
@@ -8,6 +8,7 @@ import {
   Text,
 } from '@ui-kitten/components';
 import React, {useState} from 'react';
+import {Alert, ScrollView, View} from 'react-native';
 import {FadeInView} from 'src/components/FadeinView';
 import {HandImage} from 'src/components/HandImage';
 import {HandProtocolHelp} from 'src/components/HandProtocolHelp';
@@ -23,22 +24,25 @@ const PictureIcon = (props: any): IconElement => (
   <Icon {...props} name="image-outline" />
 );
 
+const MinusIcon = (props: any): IconElement => (
+  <Icon {...props} name="minus-circle-outline" />
+);
+
 export const RiichiMahjongInputScreen = ({
   navigation,
   route,
 }: RiichiMahjongInputScreenProps) => {
-  const [handString, handUpdate] = useState<string>('');
+  const [hand, handUpdate] = useState<string>('');
+  const [huro, huroUpdate] = useState<string[][]>([]);
+  const [dora, doraUpdate] = useState<string>('');
+  const [agari, agariUpdate] = useState<string>('');
   const [inputLevel, inputLevelUpdate] = useState<number>(0);
-  const [handInformations, handInformationsUpdate] = useState<string[]>([]);
   const NextButton = () => (
     <MarginButton
       status="info"
       appearance="outline"
       onPress={() => {
         inputLevelUpdate(inputLevel + 1);
-        handInformations.push(handString);
-        handInformationsUpdate(handInformations);
-        handUpdate('');
       }}>
       다음
     </MarginButton>
@@ -47,72 +51,123 @@ export const RiichiMahjongInputScreen = ({
     <MarginButton
       status="success"
       onPress={() => {
-        handInformations.push(handString);
-        handInformationsUpdate(handInformations);
-        console.log('need api call implement');
-        console.log(`hand informations: ${handInformations}`);
         navigation.navigate('InformationInput');
       }}>
       완료
     </MarginButton>
   );
-  const changeUpdte = (text: string) => {
-    handUpdate(text);
-  };
 
   return (
     <Main>
-      <MarginInput
-        disabled={inputLevel > 0}
-        placeholder="손패 (후로 제외)"
-        onChangeText={changeUpdte}
-      />
-      {inputLevel >= 1 && (
-        <FadeInView style={{}}>
+      <ScrollView>
+        <HandView>
+          <Text>손패 (후로, 오름패 제외)</Text>
           <MarginInput
-            placeholder="후로패"
-            disabled={inputLevel > 1}
-            onChangeText={changeUpdte}
+            placeholder="ex) 1112345678999p"
+            onChangeText={(text: string) => {
+              handUpdate(text);
+            }}
           />
-        </FadeInView>
-      )}
-      {inputLevel >= 2 && (
-        <FadeInView style={{}}>
-          <MarginInput
-            placeholder="도라표지패"
-            disabled={inputLevel > 2}
-            onChangeText={changeUpdte}
-          />
-        </FadeInView>
-      )}
-      {inputLevel >= 3 && (
-        <FadeInView style={{}}>
-          <MarginInput
-            placeholder="화료패(론, 쯔모 선언패)"
-            disabled={inputLevel > 3}
-            onChangeText={changeUpdte}
-          />
-        </FadeInView>
-      )}
-      <HandImage handString={handString} />
-      <MarginButton status="basic" accessoryLeft={PictureIcon}>
-        사진으로 입력
-      </MarginButton>
-      {inputLevel === 3 ? <CompleteButton /> : <NextButton />}
-      {inputLevel >= 1 && (
-        <MarginButton
-          status="danger"
-          appearance="outline"
-          onPress={() => {
-            inputLevelUpdate(inputLevel - 1);
-            handUpdate(handInformations[handInformations.length - 1]);
-            handInformations.pop();
-            handInformationsUpdate(handInformations);
-          }}>
-          이전
+        </HandView>
+        <HandImage handString={hand} />
+        {inputLevel >= 1 && (
+          <HandView>
+            <Text>후로패</Text>
+            {huro.map((value: string[], index: number) => (
+              <View key={value[1]}>
+                <FadeInView
+                  style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <HuroInput
+                    placeholder="치퐁깡당 하나씩만, 안깡은 맨앞에 -"
+                    onChangeText={(text: string) => {
+                      huro[index][0] = text;
+                      huroUpdate([...huro]);
+                    }}
+                  />
+                  <HuroDeleteButton
+                    status="danger"
+                    appearance="ghost"
+                    accessoryLeft={MinusIcon}
+                    onPress={() => {
+                      huro.splice(index, 1);
+                      huroUpdate([...huro]);
+                    }}
+                  />
+                </FadeInView>
+                <HandImage handString={value[0]} />
+              </View>
+            ))}
+          </HandView>
+        )}
+        {inputLevel >= 1 && (
+          <MarginButton
+            status="warning"
+            appearance="outline"
+            onPress={() => {
+              huro.push(['', crypto.randomUUID()]);
+              huroUpdate([...huro]);
+            }}>
+            후로패 추가
+          </MarginButton>
+        )}
+        {inputLevel >= 2 && (
+          <HandView>
+            <Text>도라표지패</Text>
+            <FadeInView style={{}}>
+              <MarginInput
+                placeholder="아카도라는 다음 화면에서 입력"
+                onChangeText={(text: string) => {
+                  doraUpdate(text);
+                }}
+              />
+            </FadeInView>
+            <HandImage handString={dora} />
+          </HandView>
+        )}
+        {inputLevel >= 3 && (
+          <HandView>
+            <Text>화료패</Text>
+            <FadeInView style={{}}>
+              <MarginInput
+                placeholder="론, 쯔모 선언패"
+                onChangeText={(text: string) => {
+                  agariUpdate(text);
+                }}
+              />
+            </FadeInView>
+            <HandImage handString={agari} />
+          </HandView>
+        )}
+        {inputLevel === 3 ? <CompleteButton /> : <NextButton />}
+        {inputLevel >= 1 && (
+          <MarginButton
+            status="danger"
+            appearance="outline"
+            onPress={() => {
+              switch (inputLevel) {
+                case 1:
+                  huroUpdate([]);
+                  break;
+                case 2:
+                  doraUpdate('');
+                  break;
+                case 3:
+                  agariUpdate('');
+                  break;
+                default:
+                  //TODO: change it to toast
+                  console.log('invalid button level');
+              }
+              inputLevelUpdate(inputLevel - 1);
+            }}>
+            이전
+          </MarginButton>
+        )}
+        <MarginButton status="basic" accessoryLeft={PictureIcon}>
+          사진으로 입력
         </MarginButton>
-      )}
-      <HandProtocolHelp />
+        <HandProtocolHelp />
+      </ScrollView>
     </Main>
   );
 };
@@ -127,4 +182,18 @@ const MarginButton = styled(Button)`
 `;
 const MarginInput = styled(Input)`
   margin-top: 10px;
+`;
+
+const HandView = styled.View`
+  padding-top: 10px;
+`;
+
+const HuroDeleteButton = styled(Button)`
+  margin-top: 3px;
+  flex: 1;
+`;
+
+const HuroInput = styled(MarginInput)`
+  margin-top: 3px;
+  flex: 9;
 `;
